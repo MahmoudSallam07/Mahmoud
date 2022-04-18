f2128efd968d8e1f497dc2ffe83810b1e7eb096d
Mahmoud Sallam <103969812+MahmoudSallam07@users.noreply.github.com>
2022-04-19 02:27:17 +0300

    mahmoud

---
+++ app.js
@@ -0,0 +1,46 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+var typed = new Typed(".typing", {
+	strings: ["Developer", "Chemical Engineer", "Designer", "Photographer"],
+	typeSpeed: 100,
+	backSpeed: 40,
+	loop: true
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+// slides show
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) { slideIndex = 1 }
+	slides[slideIndex - 1].style.display = "block";
+	setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
